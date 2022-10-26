1f92c2be6f868307d7c9bddde72a184fdd86e170
Hary Agustian <tech.hary.agustian@gmail.com>
2022-10-27 01:51:41 +0700

    initializing day 1

---
+++ src/main/java/kawah/edukasi/springbootbatch4/SpringbootBatch4Application.java
@@ -0,0 +1,13 @@
+package kawah.edukasi.springbootbatch4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBatch4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBatch4Application.class, args);
+	}
+
+}
---
+++ src/test/java/kawah/edukasi/springbootbatch4/SpringbootBatch4ApplicationTests.java
@@ -0,0 +1,13 @@
+package kawah.edukasi.springbootbatch4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBatch4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
