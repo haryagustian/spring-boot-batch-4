a93ad0f7753f930dab951e27bd7974aaaf81434a
Hary Agustian <tech.hary.agustian@gmail.com>
2022-10-27 03:01:53 +0700

    Spring Boot Simple End Point : Day 2

--- src/main/java/kawah/edukasi/springbootbatch4/SpringbootBatch4Application.java
+++ src/main/java/kawah/edukasi/springbootbatch4/SpringbootBatch4Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootBatch4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBatch4Application.class, args);
+		SpringApplication.run(SpringbootBatch4Application.class,args);
 	}
-
 }
